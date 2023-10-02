af24563424a779216f4b128ed783da35a3aee510
Parker Siu <parker.siu@gmail.com>
2023-10-02 14:48:33 -0700

    Implemented geo coding api and updated weather api to onecall.

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,71 @@
-export default function Home() {
+async function geoCode() {
+  const city: string = "carlsbad";
+  const state: string = "ca";
+  const country: string = "us";
+  const res = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&appid=${process.env.API_KEY}`,
+    {
+      method: "GET",
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch geo data");
+  }
+  const data = await res.json();
+  return data;
+}
+const location = await geoCode();
+
+async function getWeather() {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${location[0].lat}&lon=${location[0].lon}&exclude=minutely,hourly,daily,alerts&units=imperial&appid=${process.env.API_KEY}`,
+    {
+      method: "GET",
+    }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch weather data");
+  }
+  const data = await res.json();
+  return data;
+}
+
+export default async function Home() {
+  const weather = await getWeather();
+  console.log(weather);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="container">
         <div className="left-info">
           <div className="pic-gradient"></div>
           <div className="today-info">
-            <h2>Friday</h2>
-            <span>29 July 2023</span>
+            <h2>Today</h2>
+            <span>{new Date().toLocaleDateString()}</span>
             <div>
               <i className="bx bx-current-location"></i>
-              <span>Paris, FR</span>
+              <span>{location[0].name}</span>
             </div>
           </div>
           <div className="today-weather">
             <i className="bx bx-sun"></i>
-            <h1 className="weather-temp">25°C</h1>
-            <h3>Sunny</h3>
+            <h1 className="weather-temp">
+              {Math.round(weather.current.temp)}°F
+            </h1>
+            <h3>{weather.current.weather[0].main}</h3>
           </div>
         </div>
 
         <div className="right-info">
           <div className="day-info">
             <div>
-              <span className="title">PRECIPITATION</span>
-              <span className="value">4 %</span>
-            </div>
-            <div>
               <span className="title">HUMIDITY</span>
-              <span className="value">34 %</span>
+              <span className="value">{weather.current.humidity} %</span>
             </div>
             <div>
               <span className="title">WIND SPEED</span>
-              <span className="value">6 mph</span>
+              <span className="value">
+                {Math.round(weather.current.wind_speed)} mph
+              </span>
             </div>
           </div>
 
